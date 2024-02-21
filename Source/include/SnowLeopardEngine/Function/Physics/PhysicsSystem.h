aa15cf76bae8a3612491025f3c17569ae64afd85
WangSimiao2000 <1872557359@qq.com>
2024-02-21 21:19:38 +0000

    #8 : Implement PhysicsEventCallbacks

--- Source/include/SnowLeopardEngine/Function/Physics/PhysicsSystem.h
+++ Source/include/SnowLeopardEngine/Function/Physics/PhysicsSystem.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <PxPhysicsAPI.h>
 #include "SnowLeopardEngine/Core/Base/Base.h"
 #include "SnowLeopardEngine/Core/Base/EngineSubSystem.h"
 #include "SnowLeopardEngine/Function/Physics/PhysicsErrorCallback.h"
@@ -7,13 +8,22 @@
 
 namespace SnowLeopardEngine
 {
-    class PhysicsSystem final : public EngineSubSystem
+    class PhysicsSystem final : public EngineSubSystem , public physx::PxSimulationEventCallback
     {
     public:
         DECLARE_SUBSYSTEM(PhysicsSystem)
 
         void CookPhysicsScene(const Ref<LogicScene>& logicScene);
         void OnFixedTick();
+
+        void onConstraintBreak(physx::PxConstraintInfo* constraints, physx::PxU32 count) override;
+        void onWake(physx::PxActor** actors, physx::PxU32 count) override;
+        void onSleep(physx::PxActor** actors, physx::PxU32 count) override;
+        void onContact(const physx::PxContactPairHeader& pairHeader,
+                       const physx::PxContactPair*       pairs,
+                       physx::PxU32                      count) override;
+        void onTrigger(physx::PxTriggerPair* pairs, physx::PxU32 count) override;
+        void onAdvance(const physx::PxRigidBody*const* bodyBuffer, const physx::PxTransform* poseBuffer, physx::PxU32 count) override;
 
     private:
         physx::PxDefaultAllocator m_Allocator;
