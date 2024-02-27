b1f6c828855c3b285c5e271f674b83eedfcbbd94
Lazy_V <954294627@qq.com>
2024-02-27 10:52:39 +0000

    add dockspace. update default imgui config.

--- Source/Editor/src/EditorGUISystem.cpp
+++ Source/Editor/src/EditorGUISystem.cpp
@@ -4,6 +4,7 @@
 #include "SnowLeopardEngine/Engine/EngineContext.h"
 #include "SnowLeopardEngine/Function/Rendering/GraphicsAPI.h"
 
+#include "imgui.h"
 #include <GLFW/glfw3.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
@@ -63,10 +64,51 @@ namespace SnowLeopardEngine::Editor
 
     void EditorGUISystem::OnTick(float deltaTime)
     {
+        static bool               dockSpaceOpen  = true;
+        static ImGuiDockNodeFlags dockSpaceFlags = ImGuiDockNodeFlags_None;
+
+        ImGuiWindowFlags windowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+
+        ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->Pos);
+        ImGui::SetNextWindowSize(viewport->Size);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        windowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
+                       ImGuiWindowFlags_NoMove;
+        windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+        if (dockSpaceFlags & ImGuiDockNodeFlags_PassthruCentralNode)
+        {
+            windowFlags |= ImGuiWindowFlags_NoBackground;
+        }
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpaceWindow", &dockSpaceOpen, windowFlags);
+        ImGui::PopStyleVar(3);
+
+        // DockSpace
+        ImGuiIO&    io          = ImGui::GetIO();
+        ImGuiStyle& style       = ImGui::GetStyle();
+        float       minWinSizeX = style.WindowMinSize.x;
+        float       minWinSizeY = style.WindowMinSize.y;
+        style.WindowMinSize.x   = 350.0f;
+        style.WindowMinSize.y   = 120.0f;
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            ImGuiID dockSpaceId = ImGui::GetID("DockSpace");
+            ImGui::DockSpace(dockSpaceId, ImVec2(0.0f, 0.0f), dockSpaceFlags);
+        }
+        style.WindowMinSize.x = minWinSizeX;
+        style.WindowMinSize.y = minWinSizeY;
+
         g_EngineContext->RenderSys->GetAPI()->ClearColor(glm::vec4(0, 0, 0, 0), ClearBit::Default);
         ImGui::ShowDemoWindow();
 
         PanelManager::OnTick(deltaTime);
+
+        ImGui::End();
     }
 
     void EditorGUISystem::Begin()
