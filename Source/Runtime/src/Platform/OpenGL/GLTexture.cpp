4e6543bd1639b804758edc662d9046e711e006b7
Lazy_V <954294627@qq.com>
2024-03-13 00:26:00 +0000

    add non-DSA version GL APIs.

--- Source/Runtime/src/Platform/OpenGL/GLTexture.cpp
+++ Source/Runtime/src/Platform/OpenGL/GLTexture.cpp
@@ -1,4 +1,5 @@
 #include "SnowLeopardEngine/Platform/OpenGL/GLTexture.h"
+#include "SnowLeopardEngine/Platform/OpenGL/GLAPI.h"
 
 #include <glad/glad.h>
 
@@ -53,58 +54,133 @@ namespace SnowLeopardEngine
 
         SNOW_LEOPARD_CORE_ASSERT(m_InternalFormat & m_DataFormat, "[GLTexture2D] Format not supported!");
 
-        glCreateTextures(GL_TEXTURE_2D, 1, &m_Name);
-
-        switch (desc.Config.WrapMode)
+        if (OpenGLAPI::IsDSASupported())
         {
-            case TextureWrapMode::Clamp:
-                glTextureParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-                glTextureParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-                break;
-            case TextureWrapMode::Repeat:
-                glTextureParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_REPEAT);
-                glTextureParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_REPEAT);
-                break;
-            case TextureWrapMode::Mirror:
-                glTextureParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-                glTextureParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-                break;
-            case TextureWrapMode::Invalid:
-                // Handle the case when no wrap mode is specified
-                break;
+            glCreateTextures(GL_TEXTURE_2D, 1, &m_Name);
+
+            glTextureStorage2D(m_Name, 1, m_InternalFormat, m_Desc.Width, m_Desc.Height);
+            glTextureSubImage2D(
+                m_Name, 0, 0, 0, m_Desc.Width, m_Desc.Height, m_DataFormat, GL_UNSIGNED_BYTE, data->Data);
+
+            switch (desc.Config.WrapMode)
+            {
+                case TextureWrapMode::Clamp:
+                    glTextureParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+                    glTextureParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+                    break;
+                case TextureWrapMode::Repeat:
+                    glTextureParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                    glTextureParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_REPEAT);
+                    break;
+                case TextureWrapMode::Mirror:
+                    glTextureParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+                    glTextureParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+                    break;
+                case TextureWrapMode::Invalid:
+                    // Handle the case when no wrap mode is specified
+                    break;
+            }
+
+            switch (desc.Config.FilterMode)
+            {
+                case TextureFilterMode::Point:
+                    glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                    glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    break;
+                case TextureFilterMode::Linear:
+                    glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                    glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                    break;
+                case TextureFilterMode::Nearest:
+                    glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+                    glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    break;
+                case TextureFilterMode::Invalid:
+                    // Handle the case when no filter mode is specified
+                    break;
+            }
+
+            if (desc.Config.IsGenMipMaps)
+            {
+                glGenerateTextureMipmap(m_Name);
+            }
         }
-
-        switch (desc.Config.FilterMode)
+        else
         {
-            case TextureFilterMode::Point:
-                glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-                glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-                break;
-            case TextureFilterMode::Linear:
-                glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-                glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-                break;
-            case TextureFilterMode::Nearest:
-                glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-                glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-                break;
-            case TextureFilterMode::Invalid:
-                // Handle the case when no filter mode is specified
-                break;
-        }
+            glGenTextures(1, &m_Name);
+            glBindTexture(GL_TEXTURE_2D, m_Name);
 
-        glTextureStorage2D(m_Name, 1, m_InternalFormat, m_Desc.Width, m_Desc.Height);
-        glTextureSubImage2D(m_Name, 0, 0, 0, m_Desc.Width, m_Desc.Height, m_DataFormat, GL_UNSIGNED_BYTE, data->Data);
+            glTexImage2D(GL_TEXTURE_2D,
+                         0,
+                         m_InternalFormat,
+                         m_Desc.Width,
+                         m_Desc.Height,
+                         0,
+                         m_DataFormat,
+                         GL_UNSIGNED_BYTE,
+                         data->Data);
 
-        if (desc.Config.IsGenMipMaps)
-        {
-            glGenerateTextureMipmap(m_Name);
+            switch (desc.Config.WrapMode)
+            {
+                case TextureWrapMode::Clamp:
+                    glTexParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+                    glTexParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+                    break;
+                case TextureWrapMode::Repeat:
+                    glTexParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                    glTexParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_REPEAT);
+                    break;
+                case TextureWrapMode::Mirror:
+                    glTexParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+                    glTexParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+                    break;
+                case TextureWrapMode::Invalid:
+                    // Handle the case when no wrap mode is specified
+                    break;
+            }
+
+            switch (desc.Config.FilterMode)
+            {
+                case TextureFilterMode::Point:
+                    glTexParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                    glTexParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    break;
+                case TextureFilterMode::Linear:
+                    glTexParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                    glTexParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                    break;
+                case TextureFilterMode::Nearest:
+                    glTexParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+                    glTexParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    break;
+                case TextureFilterMode::Invalid:
+                    // Handle the case when no filter mode is specified
+                    break;
+            }
+
+            if (desc.Config.IsGenMipMaps)
+            {
+                glGenerateMipmap(GL_TEXTURE_2D);
+            }
         }
     }
 
     GLTexture2D::~GLTexture2D() { glDeleteTextures(1, &m_Name); }
 
-    void GLTexture2D::Bind(uint32_t slot) const { glBindTextureUnit(slot, m_Name); }
+    void GLTexture2D::Bind(uint32_t slot) const
+    {
+        if (OpenGLAPI::IsDSASupported())
+        {
+            glBindTextureUnit(slot, m_Name);
+        }
+        else
+        {
+            glActiveTexture(GL_TEXTURE0 + slot);
+            glBindTexture(GL_TEXTURE_2D, m_Name);
+        }
+    }
+
+    void GLTexture2D::Unbind() const { glBindTexture(GL_TEXTURE_2D, 0); }
 
     GLCubemap::GLCubemap(const TextureDesc& desc, std::vector<Buffer*> dataList) : m_Desc(desc)
     {
@@ -113,55 +189,117 @@ namespace SnowLeopardEngine
 
         SNOW_LEOPARD_CORE_ASSERT(m_InternalFormat & m_DataFormat, "[GLTexture2D] Format not supported!");
 
-        glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &m_Name);
-
-        glTextureParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTextureParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTextureParameteri(m_Name, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-        switch (desc.Config.FilterMode)
+        if (OpenGLAPI::IsDSASupported())
         {
-            case TextureFilterMode::Point:
-                glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-                glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-                break;
-            case TextureFilterMode::Linear:
-                glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-                glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-                break;
-            case TextureFilterMode::Nearest:
-                glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-                glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-                break;
-            case TextureFilterMode::Invalid:
-                // Handle the case when no filter mode is specified
-                break;
+            glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &m_Name);
+
+            glTextureStorage2D(m_Name, 1, m_InternalFormat, desc.Width, desc.Height);
+
+            for (size_t face = 0; face < 6; ++face)
+            {
+                glTextureSubImage3D(m_Name,
+                                    0,
+                                    0,
+                                    0,
+                                    face,
+                                    desc.Width,
+                                    desc.Height,
+                                    1,
+                                    m_DataFormat,
+                                    GL_UNSIGNED_BYTE,
+                                    dataList[face]->Data);
+            }
+
+            glTextureParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTextureParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTextureParameteri(m_Name, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+            switch (desc.Config.FilterMode)
+            {
+                case TextureFilterMode::Point:
+                    glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                    glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    break;
+                case TextureFilterMode::Linear:
+                    glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                    glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                    break;
+                case TextureFilterMode::Nearest:
+                    glTextureParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+                    glTextureParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    break;
+                case TextureFilterMode::Invalid:
+                    // Handle the case when no filter mode is specified
+                    break;
+            }
+
+            if (desc.Config.IsGenMipMaps)
+            {
+                glGenerateTextureMipmap(m_Name);
+            }
         }
-
-        glTextureStorage2D(m_Name, 1, m_InternalFormat, desc.Width, desc.Height);
-
-        for (size_t face = 0; face < 6; ++face)
+        else
         {
-            glTextureSubImage3D(m_Name,
-                                0,
-                                0,
-                                0,
-                                face,
-                                desc.Width,
-                                desc.Height,
-                                1,
-                                m_DataFormat,
-                                GL_UNSIGNED_BYTE,
-                                dataList[face]->Data);
-        }
+            glGenTextures(1, &m_Name);
+            glBindTexture(GL_TEXTURE_CUBE_MAP, m_Name);
 
-        if (desc.Config.IsGenMipMaps)
-        {
-            glGenerateTextureMipmap(m_Name);
+            for (size_t face = 0; face < 6; ++face)
+            {
+                glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + face,
+                             0,
+                             m_InternalFormat,
+                             desc.Width,
+                             desc.Height,
+                             0,
+                             m_DataFormat,
+                             GL_UNSIGNED_BYTE,
+                             dataList[face]->Data);
+            }
+
+            glTexParameteri(m_Name, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(m_Name, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(m_Name, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+            switch (desc.Config.FilterMode)
+            {
+                case TextureFilterMode::Point:
+                    glTexParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                    glTexParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    break;
+                case TextureFilterMode::Linear:
+                    glTexParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                    glTexParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                    break;
+                case TextureFilterMode::Nearest:
+                    glTexParameteri(m_Name, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+                    glTexParameteri(m_Name, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    break;
+                case TextureFilterMode::Invalid:
+                    // Handle the case when no filter mode is specified
+                    break;
+            }
+
+            if (desc.Config.IsGenMipMaps)
+            {
+                glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+            }
         }
     }
 
     GLCubemap::~GLCubemap() { glDeleteTextures(1, &m_Name); }
 
-    void GLCubemap::Bind(uint32_t slot) const { glBindTextureUnit(slot, m_Name); }
+    void GLCubemap::Bind(uint32_t slot) const
+    {
+        if (OpenGLAPI::IsDSASupported())
+        {
+            glBindTextureUnit(slot, m_Name);
+        }
+        else
+        {
+            glActiveTexture(GL_TEXTURE0 + slot);
+            glBindTexture(GL_TEXTURE_CUBE_MAP, m_Name);
+        }
+    }
+
+    void GLCubemap::Unbind() const { glBindTexture(GL_TEXTURE_CUBE_MAP, 0); }
 } // namespace SnowLeopardEngine
--- Source/Runtime/src/Platform/OpenGL/GLVertexArray.cpp
+++ Source/Runtime/src/Platform/OpenGL/GLVertexArray.cpp
@@ -1,5 +1,6 @@
 #include "SnowLeopardEngine/Platform/OpenGL/GLVertexArray.h"
 
+#include "SnowLeopardEngine/Platform/OpenGL/GLAPI.h"
 #include "SnowLeopardEngine/Platform/OpenGL/GLIndexBuffer.h"
 #include "SnowLeopardEngine/Platform/OpenGL/GLVertexBuffer.h"
 
@@ -41,7 +42,17 @@ namespace SnowLeopardEngine
         return 0;
     }
 
-    GLVertexArray::GLVertexArray() { glCreateVertexArrays(1, &m_VertexArrayName); }
+    GLVertexArray::GLVertexArray()
+    {
+        if (OpenGLAPI::IsDSASupported())
+        {
+            glCreateVertexArrays(1, &m_VertexArrayName);
+        }
+        else
+        {
+            glGenVertexArrays(1, &m_VertexArrayName);
+        }
+    }
 
     GLVertexArray::~GLVertexArray() { glDeleteVertexArrays(1, &m_VertexArrayName); }
 
@@ -54,72 +65,143 @@ namespace SnowLeopardEngine
         SNOW_LEOPARD_CORE_ASSERT(!vertexBuffer->GetLayout().GetElements().empty(),
                                  "[Rendering][InputAssembling] Vertex Buffer has no layout!");
 
-        glVertexArrayVertexBuffer(m_VertexArrayName,
-                                  0,
-                                  std::dynamic_pointer_cast<GLVertexBuffer>(vertexBuffer)->GetName(),
-                                  0,
-                                  vertexBuffer->GetLayout().GetStride());
-        glVertexArrayElementBuffer(m_VertexArrayName, std::dynamic_pointer_cast<GLIndexBuffer>(indexBuffer)->GetName());
-
-        auto layout = vertexBuffer->GetLayout();
-        for (const auto& element : layout)
+        if (OpenGLAPI::IsDSASupported())
         {
-            switch (element.Type)
+            glVertexArrayVertexBuffer(m_VertexArrayName,
+                                      0,
+                                      std::dynamic_pointer_cast<GLVertexBuffer>(vertexBuffer)->GetName(),
+                                      0,
+                                      vertexBuffer->GetLayout().GetStride());
+            glVertexArrayElementBuffer(m_VertexArrayName,
+                                       std::dynamic_pointer_cast<GLIndexBuffer>(indexBuffer)->GetName());
+
+            auto layout = vertexBuffer->GetLayout();
+            for (const auto& element : layout)
             {
-                case ShaderDataType::Float:
-                case ShaderDataType::Float2:
-                case ShaderDataType::Float3:
-                case ShaderDataType::Float4: {
-                    glEnableVertexArrayAttrib(m_VertexArrayName, m_VertexBufferIndex);
-                    glVertexArrayAttribFormat(m_VertexArrayName,
-                                              m_VertexBufferIndex,
-                                              element.GetComponentCount(),
-                                              ShaderDataTypeToGLBaseType(element.Type),
-                                              element.Normalized ? GL_TRUE : GL_FALSE,
-                                              element.Offset);
-                    glVertexArrayAttribBinding(m_VertexArrayName, m_VertexBufferIndex, 0);
-                    m_VertexBufferIndex++;
-                    break;
-                }
-                case ShaderDataType::Int:
-                case ShaderDataType::Int2:
-                case ShaderDataType::Int3:
-                case ShaderDataType::Int4:
-                case ShaderDataType::Boolean: {
-                    glEnableVertexArrayAttrib(m_VertexArrayName, m_VertexBufferIndex);
-                    glVertexArrayAttribIFormat(m_VertexArrayName,
-                                               m_VertexBufferIndex,
-                                               element.GetComponentCount(),
-                                               ShaderDataTypeToGLBaseType(element.Type),
-                                               element.Offset);
-                    glVertexArrayAttribBinding(m_VertexArrayName, m_VertexBufferIndex, 0);
-                    m_VertexBufferIndex++;
-                    break;
-                }
-                case ShaderDataType::Mat3:
-                case ShaderDataType::Mat4: {
-                    uint8_t count = element.GetComponentCount();
-                    for (uint8_t i = 0; i < count; i++)
-                    {
+                switch (element.Type)
+                {
+                    case ShaderDataType::Float:
+                    case ShaderDataType::Float2:
+                    case ShaderDataType::Float3:
+                    case ShaderDataType::Float4: {
                         glEnableVertexArrayAttrib(m_VertexArrayName, m_VertexBufferIndex);
                         glVertexArrayAttribFormat(m_VertexArrayName,
                                                   m_VertexBufferIndex,
                                                   element.GetComponentCount(),
                                                   ShaderDataTypeToGLBaseType(element.Type),
                                                   element.Normalized ? GL_TRUE : GL_FALSE,
-                                                  element.Offset + sizeof(float) * count * i);
+                                                  element.Offset);
                         glVertexArrayAttribBinding(m_VertexArrayName, m_VertexBufferIndex, 0);
-                        glVertexAttribDivisor(m_VertexBufferIndex, 1);
                         m_VertexBufferIndex++;
+                        break;
                     }
-                    break;
+                    case ShaderDataType::Int:
+                    case ShaderDataType::Int2:
+                    case ShaderDataType::Int3:
+                    case ShaderDataType::Int4:
+                    case ShaderDataType::Boolean: {
+                        glEnableVertexArrayAttrib(m_VertexArrayName, m_VertexBufferIndex);
+                        glVertexArrayAttribIFormat(m_VertexArrayName,
+                                                   m_VertexBufferIndex,
+                                                   element.GetComponentCount(),
+                                                   ShaderDataTypeToGLBaseType(element.Type),
+                                                   element.Offset);
+                        glVertexArrayAttribBinding(m_VertexArrayName, m_VertexBufferIndex, 0);
+                        m_VertexBufferIndex++;
+                        break;
+                    }
+                    case ShaderDataType::Mat3:
+                    case ShaderDataType::Mat4: {
+                        uint8_t count = element.GetComponentCount();
+                        for (uint8_t i = 0; i < count; i++)
+                        {
+                            glEnableVertexArrayAttrib(m_VertexArrayName, m_VertexBufferIndex);
+                            glVertexArrayAttribFormat(m_VertexArrayName,
+                                                      m_VertexBufferIndex,
+                                                      element.GetComponentCount(),
+                                                      ShaderDataTypeToGLBaseType(element.Type),
+                                                      element.Normalized ? GL_TRUE : GL_FALSE,
+                                                      element.Offset + sizeof(float) * count * i);
+                            glVertexArrayAttribBinding(m_VertexArrayName, m_VertexBufferIndex, 0);
+                            glVertexAttribDivisor(m_VertexBufferIndex, 1);
+                            m_VertexBufferIndex++;
+                        }
+                        break;
+                    }
+                    default:
+                        SNOW_LEOPARD_CORE_ASSERT(false, "[Rendering][InputAssembling] Unknown ShaderDataType!");
                 }
-                default:
-                    SNOW_LEOPARD_CORE_ASSERT(false, "[Rendering][InputAssembling] Unknown ShaderDataType!");
             }
-        }
 
-        m_VertexBuffers.push_back(vertexBuffer);
-        m_IndexBuffer = indexBuffer;
+            m_VertexBuffers.push_back(vertexBuffer);
+            m_IndexBuffer = indexBuffer;
+        }
+        else
+        {
+            glBindVertexArray(m_VertexArrayName);
+            vertexBuffer->Bind();
+
+            auto layout = vertexBuffer->GetLayout();
+            for (const auto& element : layout)
+            {
+                switch (element.Type)
+                {
+                    case ShaderDataType::Float:
+                    case ShaderDataType::Float2:
+                    case ShaderDataType::Float3:
+                    case ShaderDataType::Float4: {
+                        glEnableVertexAttribArray(m_VertexBufferIndex);
+                        glVertexAttribPointer(m_VertexBufferIndex,
+                                              element.GetComponentCount(),
+                                              ShaderDataTypeToGLBaseType(element.Type),
+                                              element.Normalized ? GL_TRUE : GL_FALSE,
+                                              layout.GetStride(),
+                                              (const void*)(element.Offset));
+                        m_VertexBufferIndex++;
+                        break;
+                    }
+                    case ShaderDataType::Int:
+                    case ShaderDataType::Int2:
+                    case ShaderDataType::Int3:
+                    case ShaderDataType::Int4:
+                    case ShaderDataType::Boolean: {
+                        glEnableVertexAttribArray(m_VertexBufferIndex);
+                        glVertexAttribIPointer(m_VertexBufferIndex,
+                                               element.GetComponentCount(),
+                                               ShaderDataTypeToGLBaseType(element.Type),
+                                               layout.GetStride(),
+                                               (const void*)(element.Offset));
+                        m_VertexBufferIndex++;
+                        break;
+                    }
+                    case ShaderDataType::Mat3:
+                    case ShaderDataType::Mat4: {
+                        uint8_t count = element.GetComponentCount();
+                        for (uint8_t i = 0; i < count; i++)
+                        {
+                            glEnableVertexAttribArray(m_VertexBufferIndex);
+                            glVertexAttribPointer(m_VertexBufferIndex,
+                                                  count,
+                                                  ShaderDataTypeToGLBaseType(element.Type),
+                                                  element.Normalized ? GL_TRUE : GL_FALSE,
+                                                  layout.GetStride(),
+                                                  (const void*)(element.Offset + sizeof(float) * count * i));
+                            glVertexAttribDivisor(m_VertexBufferIndex, 1);
+                            m_VertexBufferIndex++;
+                        }
+                        break;
+                    }
+                    default:
+                        SNOW_LEOPARD_CORE_ASSERT(false, "Unknown ShaderDataType!");
+                }
+            }
+
+            m_VertexBuffers.push_back(vertexBuffer);
+
+            glBindVertexArray(m_VertexArrayName);
+            indexBuffer->Bind();
+
+            m_IndexBuffer = indexBuffer;
+        }
     }
 } // namespace SnowLeopardEngine
