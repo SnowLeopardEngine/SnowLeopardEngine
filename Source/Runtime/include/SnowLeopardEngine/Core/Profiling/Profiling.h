561829ac1767f478e08c78edd03d9446a9873b3f
Lazy_V <954294627@qq.com>
2024-03-23 13:39:52 +0000

    refactor: add data driven pipeline

--- Source/Runtime/include/SnowLeopardEngine/Core/Profiling/Profiling.h
+++ Source/Runtime/include/SnowLeopardEngine/Core/Profiling/Profiling.h
@@ -3,4 +3,5 @@
 // Profiling
 #include <tracy/Tracy.hpp>
 #define SNOW_LEOPARD_PROFILE_FUNCTION ZoneScoped;
+#define SNOW_LEOPARD_PROFILE_NAMED_SCOPE(__VA_ARGS__) ZoneScopedN(__VA_ARGS__);
 #define SNOW_LEOPARD_PROFILE_END_OF_FRAME FrameMark;
