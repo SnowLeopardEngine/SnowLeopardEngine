d5a5f4de57ea76434783e36f00de05f086a6a5b8
Leo Langer <leolanger2001@outlook.com>
2024-03-25 03:22:12 +0000

    Animator Controller (Second Version)

--- Source/Runtime/include/SnowLeopardEngine/Function/Animation/AnimatorController.h
+++ Source/Runtime/include/SnowLeopardEngine/Function/Animation/AnimatorController.h
@@ -2,6 +2,7 @@
 
 #include "SnowLeopardEngine/Function/Animation/Animator.h"
 #include "SnowLeopardEngine/Function/Animation/Transition.h"
+#include <variant>
 
 namespace SnowLeopardEngine
 {
@@ -11,27 +12,33 @@ namespace SnowLeopardEngine
         void RegisterAnimator(const Ref<Animator>& animator);
         void DeleteAnimator(const Ref<Animator>& animator);
 
-        void RegisterTransition(const Ref<Animator>& sourceAnimator,
-                                const Ref<Animator>& targetAnimator,
-                                int                  duration,
-                                std::string          triggerName);
+        void RegisterTransition(const Ref<Animator>& sourceAnimator, const Ref<Animator>& targetAnimator, int duration);
 
-        void DeleteTransition(const Ref<Animator>& sourceAnimator, std::string triggerName);
+        void DeleteTransition(const Ref<Animator>& sourceAnimator, const Ref<Animator>& targetAnimator);
+
+        void RegisterParameters(const std::string& parameterName, const std::variant<float, bool>& defaultValue);
+        void RegisterParameters(const std::string& parameterName);
+        void DeleteParameters(const std::string& parameterName);
 
         void Blending(const Ref<Animator>& sourceAnimator, const Ref<Animator>& targetAnimator);
 
         void SetEntryAnimator(const Ref<Animator>& animator);
 
-        void SetTrigger(std::string triggerName);
+        void SetTrigger(const std::string& triggerName);
+        void SetFloat(const std::string& floatName, float value);
+        void SetBoolean(const std::string& booleanName, bool value);
 
         void InitAnimators();
         void UpdateAnimators(float deltaTime);
 
+        void CheckParameters();
+
         inline Ref<Animator> GetCurrentAnimator() const { return m_CurrentAnimator; }
 
     private:
-        Ref<Animator>                                         m_CurrentAnimator;
-        std::vector<Ref<Animator>>                            m_Animators;
-        std::map<Ref<Animator>, std::vector<Ref<Transition>>> m_TransitionsInfoMap;
+        Ref<Animator>                                                         m_CurrentAnimator;
+        std::vector<Ref<Animator>>                                            m_Animators;
+        std::map<Ref<Animator>, std::vector<Ref<Transition>>>                 m_TransitionsInfoMap;
+        Ref<std::map<std::string, std::variant<float, bool, std::monostate>>> m_Parameters;
     };
 } // namespace SnowLeopardEngine
