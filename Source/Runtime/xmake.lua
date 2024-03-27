8cb0bfe858b80b337b36e9f86373fdb9e1670627
Lazy_V <954294627@qq.com>
2024-03-27 04:18:27 +0000

    remove assimp. fix workflows.

--- Source/Runtime/xmake.lua
+++ Source/Runtime/xmake.lua
@@ -2,9 +2,7 @@
 add_requires("spdlog", "fmt 9.1.0", "glfw", "glm", "stduuid", "entt v3.10.3")
 add_requires("miniaudio", "stb", "tracy", "magic_enum", "cereal", "shaderc", "spirv-cross", "scoral", "cryptopp")
 add_requires("sphysx-cross", { configs = { debug = is_mode("debug") }})
-add_requireconfs("assimp.zlib", {system = false}) -- https://github.com/xmake-io/xmake-repo/issues/1855
 add_requires("ozz-animation", { configs = { shared = true }})
-add_requires("assimp v5.0.1", { configs = { debug = is_mode("debug") }})
 
 -- target defination, name: SnowLeopardEngine
 target("SnowLeopardEngine")
@@ -39,7 +37,6 @@ target("SnowLeopardEngine")
     add_packages("spirv-cross", { public = true })
     add_packages("scoral", { public = true })
     add_packages("cryptopp", { public = true })
-    add_packages("assimp", { public = true })
     add_packages("ozz-animation", { public = true })
     add_packages("sphysx-cross", { public = true })
 
